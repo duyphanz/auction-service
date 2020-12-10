44082a31ddc586c676bb5d4893a18dc5de08c1b4
Duy Phan <duyphan@lecle.co.kr>
2020-12-10 15:56:01 +0700

    feat: implement create auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,13 +1,26 @@
-/* eslint-disable-next-line */
-async function createAuction(event, context) {
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
+
+  await dynamoDB
+    .put({
+      TableName: process.env.AUCTION_TABLE_NAME,
+      Item: auction,
+    })
+    .promise();
+
   return {
     statusCode: 200,
     body: JSON.stringify({ auction }),
