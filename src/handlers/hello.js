a40ec2e05d94801dd7596ac6cde319025428f426
Duy Phan <duyphan@lecle.co.kr>
2020-12-10 14:35:40 +0700

    feat: create auctions table

--- src/handlers/hello.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable-next-line */
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello world!' }),
-  };
-}
-
-export const handler = hello;
